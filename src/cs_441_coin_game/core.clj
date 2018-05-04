84444c6c374da4572d0e68629d0a2b8387bd2401
Jeet Das <ayushmandas@hotmail.com>
2018-05-03 21:15:50 -0500

    Basic implementation of strategy for player 1

--- src/cs_441_coin_game/core.clj
+++ src/cs_441_coin_game/core.clj
@@ -6,16 +6,32 @@
 (def sum2 0)
 
 (defn readFile [fileName]
-	(with-open [rdr (clojure.java.io/reader (str "/Users/jeet/Workspace/Clojure_projects/cs-441-coin-game/resources/" fileName ".txt"))](clojure.string/join "\n" (line-seq rdr))))
+	(with-open [rdr (clojure.java.io/reader (str "/Users/jeet/Workspace/Clojure_projects/cs-441-coin-game/resources/" fileName ".txt"))](clojure.string/join "\n" (line-seq rdr)))
+)
 
 (defn convertStringToList [numbers]
-	(drop 1 (map #(Integer/parseInt %)(cstr/split numbers #" "))))
+	(drop 1 (map #(Integer/parseInt %)(cstr/split numbers #" ")))
+)
 
 (defn process [numbers evenSum oddSum]
-	(println numbers evenSum oddSum))
+	(println numbers evenSum oddSum)
+	(if (even? (count numbers))
+		(if (> evenSum oddSum)
+			(def sum1 (+ sum1 (first (reverse numbers))))
+			(def sum1 (+ sum1 (first numbers)))
+		)
+		(if (> (first numbers) (first (reverse numbers)))
+			(def sum1 (+ sum1 (first numbers)))
+			(def sum1 (+ sum1 (first (reverse numbers))))
+		)
+	)
+)
 
 (defn -main [& args]
-	(def numbersStringList (readFile "10"))
+	(def numbersStringList (readFile 10))
 	(def numbers (convertStringToList numbersStringList))
-	(process numbers 0 0)
+	(def evenSum (reduce + (vec (take-nth 2 (rest numbers)))))
+	(def oddSum (reduce + (vec (take-nth 2 (vec numbers)))))
+	(process numbers evenSum oddSum)
+	(println sum1)
 )
