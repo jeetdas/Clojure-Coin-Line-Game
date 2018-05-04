8f09b04407824d06139d73abc7df4b305cecf6a3
Jeet Das <ayushmandas@hotmail.com>
2018-05-04 13:53:28 -0500

    Implementation allows player 1 to play strategy and player 2 to minimize loss

--- src/cs_441_coin_game/core.clj
+++ src/cs_441_coin_game/core.clj
@@ -13,25 +13,79 @@
 	(drop 1 (map #(Integer/parseInt %)(cstr/split numbers #" ")))
 )
 
-(defn process [numbers evenSum oddSum]
-	(println numbers evenSum oddSum)
-	(if (even? (count numbers))
-		(if (> evenSum oddSum)
-			(def sum1 (+ sum1 (first (reverse numbers))))
-			(def sum1 (+ sum1 (first numbers)))
+(declare processOddNumbers)
+(declare processOddNumbersP2)
+
+;; Current implementation
+;; ------------------------------------------------------------------------------------------------------------------
+
+(defn processEvenNumbersP1 [numbers]
+	;;(println "EVEN NUMBERS FUN = " numbers)
+	(def evenSum (reduce + (vec (take-nth 2 (rest numbers)))))
+	(def oddSum (reduce + (vec (take-nth 2 (vec numbers)))))
+
+	;;(println oddSum evenSum numbers)
+
+	(if (> evenSum oddSum)
+		(do
+			;;(println "Winner = " (last numbers))
+			1
 		)
-		(if (> (first numbers) (first (reverse numbers)))
-			(def sum1 (+ sum1 (first numbers)))
-			(def sum1 (+ sum1 (first (reverse numbers))))
+		(do
+			;;(println "Winner = " (first numbers))
+			0
+		)
+	)
+)
+
+(defn processEvenNumbersP2 [numbers]
+	(def winValue (processEvenNumbersP1 numbers))
+	(if (= winValue 0)
+		(+ (first numbers) (processOddNumbersP2 (drop 1 numbers)) )
+		(+ (last numbers) (processOddNumbersP2 (drop-last numbers)) )
+	)
+)
+
+(defn processOddNumbersP2 [numbers]
+	(if (<= 1 (count numbers))
+		0
+		(do
+			(max (processEvenNumbersP2 (drop 1 numbers)) (processEvenNumbersP2 (drop-last numbers)) )
+		)
+	)
+)
+
+(defn processOddNumbers[numbers]
+	(def lval (processEvenNumbersP2 (drop 1 numbers)) )
+	(def rval (processEvenNumbersP2 (drop-last numbers)) )
+	(if (> lval rval)
+		(last numbers)
+		(first numbers)
+	)
+)
+
+(defn handlerMethod [numbers]
+	(if (even? (count numbers))
+		(do
+			(def res (processEvenNumbersP1 numbers))
+			res
+		)
+		(do
+			(def res (processOddNumbers numbers))
+			res
 		)
 	)
 )
 
 (defn -main [& args]
 	(def numbersStringList (readFile 10))
-	(def numbers (convertStringToList numbersStringList))
+	;;(def numbers (convertStringToList numbersStringList))
+	(def numbers '(1 2 3 6 5 4))
 	(def evenSum (reduce + (vec (take-nth 2 (rest numbers)))))
 	(def oddSum (reduce + (vec (take-nth 2 (vec numbers)))))
-	(process numbers evenSum oddSum)
-	(println sum1)
+	(println "PICK" (handlerMethod '(71 83 6 37 35 94 1 11 8)) "GIVEN (71 83 6 37 35 94 1 11 8)")
+	;;(println (process2 numbers evenSum oddSum 0))
+	;;(process numbers evenSum oddSum)
+	;;(println sum1)
+
 )
